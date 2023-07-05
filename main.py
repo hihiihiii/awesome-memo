8b2eb249175c31c38f86fd411b886d26600c060a
hihiihiii <ahrthfla@naver.com>
2023-07-05 22:41:51 +0900

    python mongDB 연결
    python mongDB 연결

--- main.py
+++ main.py
@@ -1,26 +1,78 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+from pymongo import MongoClient
+
+
+client = MongoClient(
+    "mongodb+srv://a96689636:#3gjdirhtn@cluster0.aqcybvg.mongodb.net/?retryWrites=true&w=majority"
+)
+
+db = client.python_test
+
+
+# 생성
+# doc = {"name": "kim", "age": 19}
+# db.users.insert_one(doc)
 
 
 class Memo(BaseModel):
-    id:str
+    id: str
     content: str
-    
-memos =[]
+
+
+# memos = []
 app = FastAPI()
 
-@app.post('/memos')
-def create_memo(memo:Memo):
-    memos.append(memo)
+
+# 메모 생성
+@app.post("/memos")
+def create_memo(memo: Memo):
+    doc = {"id": memo.id, "content": memo.content}
+    db.memos.insert_one(doc)
     return "성공했습니다."
 
 
-@app.get('/memos')
+# 메모 불러오기
+@app.get("/memos")
 def read_memo():
-    return memos
+    memo = list(db.memos.find({}, {"_id": False}))
+    return memo
 
-app.mount("/", StaticFiles(directory="static",html=True), name="static")
+
+# 메모 수정
+@app.put("/memos/{memo_id}")
+def edit_memo(req_memo: Memo):
+    print(req_memo.id, req_memo.content)
+
+    db.memos.update_one({"id": req_memo.id}, {"$set": {"content": req_memo.content}})
+
+    # for memo in memos:
+    #     if memo.id == req_memo.id:
+    #         memo.content = req_memo.content
+    #         return "성공했습니다."
+    return "그런 메모는 없습니다."
 
 
-    
+# 메모 삭제
+@app.delete("/memos/{memo_id}")
+def delete_memo(memo_id):
+    db.memos.delete_one({"id": memo_id})
+    # for index, memo in enumerate(memos):
+    #     if memo.id == memo_id:
+    #         memos.pop(index)
+    return "성공했습니다."
+
+
+@app.get("/memos?sort_by=created&sorted=ASC")
+def sort_asc_memo():
+    memos.id.sort()
+    return "성공했습니다."
+
+
+@app.get("/memos?sort_by=created&sorted=DESC")
+def sort_desc_memo():
+    return "성공했습니다."
+
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
--- static/app.js
+++ static/app.js
@@ -1,9 +1,46 @@
 const ul = document.querySelector("#memo-ul");
 
+async function delMemo(e) {
+  const id = e.target.dataset.id;
+  if (window.confirm("삭제하시겠습니까?")) {
+    const res = await fetch(`/memos/${id}`, {
+      method: "DELETE",
+    });
+  }
+  readMemo();
+}
+
+async function editMemo(e) {
+  const id = e.target.dataset.id;
+  const editInput = prompt("수정할 값을 입력하세요~");
+  const res = await fetch(`/memos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id, content: editInput }),
+  });
+  readMemo();
+}
+
 function displayMemos(memo) {
   const li = document.createElement("li");
+  const editBtn = document.createElement("button");
+  const delBtn = document.createElement("button");
+
   li.innerText = `[id:${memo.id} ${memo.content}]`;
+
+  editBtn.innerText = "수정하기";
+  editBtn.addEventListener("click", editMemo);
+  editBtn.dataset.id = memo.id;
+
+  delBtn.innerText = "삭제하기";
+  delBtn.addEventListener("click", delMemo);
+  delBtn.dataset.id = memo.id;
+
   ul.appendChild(li);
+  li.appendChild(editBtn);
+  li.appendChild(delBtn);
 }
 
 async function readMemo() {
